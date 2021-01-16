cc33f9b77225203d5b2351cf2be72470243b52a8
Luca <shaohsiung@163.com>
2021-01-16 10:20:57 +0800

    📃 文件重命名

--- baidu.py
+++ baidu.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 
 BAIDU_ROOT_URL = 'https://www.baidu.com/'
-baidu_hot_search_url = 'http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=baidu&fenlei=256&rsv_pq=8c475a4100039a9a&rsv_t=98e0bG6P16UQ5U9Y8uEvs3S3c1Le7P52pP7DjOoQb56LpYtxYJtFVHHtgJQ&rqlang=cn&rsv_dl=tb&rsv_enter=1&rsv_sug3=7&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug=baidu&rsp=0&inputT=1052&rsv_sug4=3762'
+BAIDU_URL = 'http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=baidu&fenlei=256&rsv_pq' \
+                       '=8c475a4100039a9a&rsv_t=98e0bG6P16UQ5U9Y8uEvs3S3c1Le7P52pP7DjOoQb56LpYtxYJtFVHHtgJQ&rqlang=cn' \
+                       '&rsv_dl=tb&rsv_enter=1&rsv_sug3=7&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug' \
+                       '=baidu&rsp=0&inputT=1052&rsv_sug4=3762 '
 
-res = requests.get(baidu_hot_search_url)
+res = requests.get(BAIDU_URL)
 status = res.status_code
 if res.status_code == 200:
     soup = BeautifulSoup(res.content, 'html.parser')
