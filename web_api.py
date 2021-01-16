82d7a991338bf9c31ddd66d932de35721a7af280
Luca <yangshaoxiong5545@gmail.com>
2021-01-16 16:31:58 +0800

    修复使用手机浏览器访问时，由于响应头没有设置字符集导致的中文乱码

---
+++ web_api.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, make_response
+import os
+
+from weibo import do as weibo_do
+from baidu import do as baidu_do
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    weibo_items = weibo_do()
+    baidu_items = baidu_do()
+    result = {'微博热搜': weibo_items, '百度热榜': baidu_items}
+    # todo UI
+    response = make_response(result)
+    response.status = '200'
+    response.headers['Content-Type'] = 'application/json;charset=utf-8'
+    return response
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.config['JSON_AS_ASCII'] = False
+    app.run(debug=False, host='0.0.0.0', port=port)
